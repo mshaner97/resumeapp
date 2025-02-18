9b5359d402de95914a3e13122266e8642bc8a73a
mshaner97 <mshaner97@icloud.com>
2025-02-18 17:39:46 -0600

    branch difficulties, problem solving.

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,21 @@ function App() {
 
   return (
     <>
-      <div></div>
+      <header>
+        <div>MENU</div>
+        <div>PROJ. 1</div>
+        <div>PROJ. 2</div>
+        <div>PROJ. 3</div>
+        <div>SEARCHBAR</div>
+      </header>
+      <div>
+        <div className='picturebox'>PICTURE</div>
+        <div className='biobox'>BIO</div>
+      </div>
+      <div> 3: links and whatnot?</div>
     </>
   )
 }
 
 export default App
+
